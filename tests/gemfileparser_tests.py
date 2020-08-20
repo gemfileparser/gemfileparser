90bedddbc01dd48a0a7a12fef40e66790065ba1f
Philippe Ombredanne <pombredanne@nexb.com>
2020-08-20 14:14:41 +0200

    Make tests pass. Remove nose dependency
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- tests/gemfileparser_tests.py
+++ tests/gemfileparser_tests.py
@@ -3,7 +3,7 @@
 # Copyright 2015 Balasankar C <balasankarc@autistici.org>
 
 # gemfileparser is dual-licensed under [GNU GPL version 3 (or above) License]
-# (http://www.gnu.org/licenses/gpl) 
+# (http://www.gnu.org/licenses/gpl)
 # and [MIT License](https://opensource.org/licenses/MIT).
 
 # Personally, I prefer anyone using this to respect the GPL license and use that
@@ -23,7 +23,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 # MIT License
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -32,10 +31,10 @@
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -44,56 +43,56 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from nose.tools import assert_equal, assert_in
 from gemfileparser import GemfileParser
 
 
 def test_source_only_gemfile():
     gemparser = GemfileParser('tests/Gemfile')
     expected = {
+        'dependency': [],
         'development': [],
         'test': [],
-        'runtime': [],
         'metrics': [],
-        'production': []}
+        'runtime': [],
+        'production': [],
+    }
     dependencies = gemparser.parse()
-    assert_equal(dependencies, expected)
+    assert expected == dependencies
 
 
 def test_name():
     gemparser = GemfileParser('tests/Gemfile_2')
     dependencies = gemparser.parse()
-    assert_equal(dependencies['runtime'][0].name, 'rails')
+    assert 'rails' == dependencies['runtime'][0].name
 
 
 def test_requirement():
     gemparser = GemfileParser('tests/Gemfile_2')
     dependencies = gemparser.parse()
-    assert_equal(dependencies['runtime'][0].requirement, '4.2.4')
+    assert ['4.2.4'] == dependencies['runtime'][0].requirement
 
 
 def test_group():
     gemparser = GemfileParser('tests/Gemfile_3')
     dependencies = gemparser.parse()
-    assert_equal(dependencies['development'][0].requirement, '4.2.4')
+    assert ['4.2.4'] == dependencies['development'][0].requirement
 
 
 def test_group_block():
     gemparser = GemfileParser('tests/Gemfile_2')
     dependencies = gemparser.parse()
-    assert_equal(dependencies['development'][0].requirement, '3.0.0')
-    assert_equal(dependencies['runtime'][0].requirement, '4.2.4')
+    assert ['3.0.0'] == dependencies['development'][0].requirement
+    assert ['4.2.4'] == dependencies['runtime'][0].requirement
 
 
 def test_source():
     gemparser = GemfileParser('tests/Gemfile_2')
     dependencies = gemparser.parse()
-    assert_equal(dependencies['runtime'][0].source,
-                 'http://www.example.com')
+    assert 'http://www.example.com' == dependencies['runtime'][0].source
 
 
 def test_gemspec():
     gemparser = GemfileParser('tests/Gemfile_2')
     dependencies = gemparser.parse()
-    assert_in('rails', [x.name for x in dependencies['runtime']])
-    assert_in('responders', [x.name for x in dependencies['development']])
+    assert 'rails' in [x.name for x in dependencies['runtime']]
+    assert 'responders' in [x.name for x in dependencies['development']]
